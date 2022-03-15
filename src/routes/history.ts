e9ae87ba67f5490ca30618b9245e8a6e22e85d0e
Sergey Dyakov <s.dyakov9@gmail.com>
2022-03-16 02:54:09 +0300

    Final preparation for connecting the network of things.

--- src/routes/history.ts
+++ src/routes/history.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { Server } from 'ws';
 
 export function getHistory(req: Request, res: Response) {
-  const websocket: Server = req.app.get('websocket');
-  websocket.emit('history', req.body);
+  const websocket: WebSocket = req.app.get('websocket');
+  websocket.send(JSON.stringify(req.body));
   res.status(200).end();
 }
